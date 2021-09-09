596d16208e10fc07457e17449706b0966ee883ee
dedeklender <43282734+dedeklender@users.noreply.github.com>
2021-09-09 11:27:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
-# bot.py
-import os
-
 import discord
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+import os
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-print(TOKEN)
-client.run(TOKEN)
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
